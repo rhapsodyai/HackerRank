7144bbd37a3f7938b0a301540d28ac61f546cda5
rhapsodyai <rhapsody.ai@gmail.com>
2017-05-23 17:22:43 -0700

    day 2

---
+++ 30DaysOfCode/src/ThirtyDaysOfCode2.java
@@ -0,0 +1,26 @@
+/**
+ * Created by kiyomizumia on 2017/05/23.
+ */
+
+import java.util.*;
+import java.math.*;
+
+public class ThirtyDaysOfCode2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+
+        // Write your calculation code here.
+        double totalCostDouble = mealCost + (mealCost * tipPercent / 100) + (mealCost * taxPercent / 100);
+
+
+        // cast the result of the rounding operation to an int and save it as totalCost
+        int totalCost = (int) Math.round(totalCostDouble);
+
+        // Print your result
+        System.out.println("The total meal cost is " + totalCost + " dollars.");
+    }
+}
