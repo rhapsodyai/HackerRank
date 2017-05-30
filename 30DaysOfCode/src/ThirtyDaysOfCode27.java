84b20833e5abcd6f452fdf684eb42f9185399624
rhapsodyai <rhapsody.ai@gmail.com>
2017-05-29 23:33:16 -0700

    up to day 27

--- 30DaysOfCode/src/ThirtyDaysOfCode27.java
+++ 30DaysOfCode/src/ThirtyDaysOfCode27.java
@@ -1,5 +1,26 @@
 /**
  * Created by kiyomizumia on 2017/05/26.
  */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class ThirtyDaysOfCode27 {
+
+    public static void main(String[] args) {
+        System.out.println(5);
+        System.out.println("3 3");
+        System.out.println("0 -3 4");
+        System.out.println("4 2");
+        System.out.println("0 -1 2 1");
+        System.out.println("5 2");
+        System.out.println("-1 0 1 0 0");
+        System.out.println("10 5");
+        System.out.println("0 1 2 3 4 5 6 7 8 9");
+        System.out.println("6 3");
+        System.out.println("7 7 6 6 5 5 0 1 2 3");
+    }
 }
